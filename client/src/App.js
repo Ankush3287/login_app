5b86f72caea63b607c8008c32b42c0c11b30fbac
ankush <ankushsolanki3287@gmail.com>
2023-01-03 15:43:22 +0530

    Added username component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,51 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-function App() {
+/** Import all Components */
+import PageNotFound from "./components/PageNotFound";
+import Password from "./components/Password";
+import Profile from "./components/Profile";
+import Recovery from "./components/Recovery";
+import Register from "./components/Register";
+import Username from "./components/Username";
+import Reset from "./components/reset";
+
+// Root Routes
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Username />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/password",
+    element: <Password />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+  {
+    path: "/recovery",
+    element: <Recovery />,
+  },
+  {
+    path: "/reset",
+    element: <Reset />,
+  },
+  {
+    path: "*",
+    element: <PageNotFound />,
+  },
+]);
+
+export default function App() {
   return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
   );
 }
-
-export default App;
